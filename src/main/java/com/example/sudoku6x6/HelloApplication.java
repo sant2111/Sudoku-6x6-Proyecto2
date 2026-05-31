f8d43aaf09062156efd07a8acfb8c97beaa18e03
sant2111 <s4nt2ll1@gmail.com>
2026-05-31 18:06:41 -0500

    added template for sudoku6x6 main window

--- src/main/java/com/example/sudoku6x6/HelloApplication.java
+++ src/main/java/com/example/sudoku6x6/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sudoku-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
+        stage.setTitle("Sudoku");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/sudoku6x6/HelloController.java
+++ src/main/java/com/example/sudoku6x6/HelloController.java
@@ -1,14 +1,52 @@
 package com.example.sudoku6x6;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+public class HelloController  implements Initializable {
+
+   @FXML
+    private GridPane sudokuContainer;
+
+   @Override
+    public void initialize(URL url , ResourceBundle resourceBundle) {
+        createSudoku();
+   }
+
+   private void createSudoku() {
+
+       for(int row = 0; row < 6; row++) {
+           for(int column = 0; column < 6; column++) {
+
+               TextField sudokuCell = new TextField();
+               sudokuCell.setAlignment(Pos.CENTER);
+               sudokuCell.setPrefSize(70,70);
+               sudokuCell.setStyle("-fx-font-size: 30px;");
+
+
+
+
+               sudokuContainer.add(sudokuCell,column,row);
+           }
+
+
+       }
+
+
+   }
+
+
 }
+
+
+
+
+
